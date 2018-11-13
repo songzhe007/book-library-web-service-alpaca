54129b4f4b27039a71b0a740d34b1660d1d3a951
Song Zhe <songzhewhu@gmail.com>
2018-11-13 15:27:06 -0800

    Zhe Song's Lab3

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,6 +10,14 @@ def index():
 if __name__ == '__main__':
     app.run()
 
+@app.route('/zheLab3')
+def zheLab3():
+	return 'This is Zhe Song lab3'
+
+
+@app.route('/jinhaoLab3')
+def jinhaoLab3():
+	return 'This is Jinhao Liu lab3'
 
 @app.route('/anranLab3')
 def anranLab3():
